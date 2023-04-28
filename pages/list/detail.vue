43dedde1511e053a6b33c5c55098a7afe1a75186
bangbangcen <530499229@qq.com>
2023-04-28 20:51:20 +0800

    con: 项目搭建

---
+++ pages/list/detail.vue
@@ -0,0 +1,48 @@
+<template>
+
+</template>
+
+<script>
+	export default {
+		// #ifdef APP
+		components:{
+			"uni-nav-bar":uniNavBar
+		},
+		onBackPress({from}) {
+			if(from == 'backbutton'){
+				if(uniShare.isShow){
+					this.$nextTick(function(){
+						console.log(uniShare);
+						uniShare.hide()
+					})
+				}
+				return uniShare.isShow;
+			}
+		},
+		// #endif
+		data() {
+			return {
+
+			}
+		},
+		computed: {
+		},
+		onLoad(event) {
+		},
+		onReady() {
+
+		},
+		onNavigationBarButtonTap(event) {
+			if (event.type == 'share') {
+				this.shareClick();
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ utils/axios/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const config = {
+	baseURL: 'http://127.0.0.1:3000/',
+	changeOrigin: true
+};
+
+const $axios = axios.create(config);
+
+$axios.interceptors.request.use((req) => {
+	req.headers.set("token", localStorage.getItem("token"));
+	return req;
+});
+
+$axios.interceptors.response.use((res) => {
+	return res.data;
+}, (err) => {
+	const {
+		response: {
+			status,
+			data: {
+				message
+			}
+		}
+	} = err;
+	if (status === 401) {
+		proxy.$Modal.error({
+			content: message,
+			title: "认证失败",
+			onOk: () => {
+				router.push('/login');
+			}
+		});
+	}
+});
+
+export default $axios;
